c2accadcaf197a9c07cf3640a154fd1fa730f7b9
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2024-10-01 06:11:32 +0100

    Update Projects.ts

--- src/app/assets/data/Projects.ts
+++ src/app/assets/data/Projects.ts
@@ -1,7 +1,20 @@
 import { StaticImageData } from 'next/image';
 import Image from '../images/placeholder.jpg';
 import InfinityShadow from '../images/1.jpg'
-
+import ChooseNPick from '../images/2.jpg';
+import JapanSurgicals from '../images/3.jpg'
+import ChoosenPick from '../images/4.jpg'
+import QuranTune from '../images/5.jpg'
+import HomeInternetPlains from '../images/6.jpg'
+import HighAccessRentals from '../images/7.jpg'
+import SultanOfMartialArt from '../images/8.jpg'
+import DigitalWatch from '../images/9.jpg'
+import ReactCalculator from '../images/10.jpg'
+import PharmacyManagementSystem from '../images/11.jpg'
+import WeatherApp from '../images/12.jpg'
+import TicTacToe from '../images/13.jpg'
+import CounterApp from "../images/14.jpg"
+import ZenithIDE from '../images/15.jpg'
 export interface IProjects {
     title: string;
     description: string;
@@ -27,7 +40,7 @@ const projects: IProjects[] = [
         description: "An online store built with React and Node.js, featuring a robust shopping cart and payment integration. Users can browse products, add them to their cart, and complete their purchases with ease. The store is optimized for mobile devices and includes user authentication, allowing customers to create accounts, track orders, and manage their profiles seamlessly.",
         category: "Web Development",
         image: {
-            src: Image,
+            src: ChooseNPick,
         },
         github: "https://github.com/shawaiz-khan",
         demo: "https://devdigitals.org/",
@@ -37,7 +50,7 @@ const projects: IProjects[] = [
         description: "A blogging platform developed using Next.js that allows users to create, edit, and manage their posts. The platform features a rich text editor, user authentication, and a comment section for reader engagement. Users can categorize their blogs, add tags, and enhance their content with images and videos, providing a user-friendly interface for both writers and readers.",
         category: "Web Development",
         image: {
-            src: Image,
+            src: JapanSurgicals,
         },
         github: "https://github.com/shawaiz-khan",
         demo: "https://devdigitals.org/",
@@ -47,7 +60,7 @@ const projects: IProjects[] = [
         description: "A task management application built with React and Tailwind CSS to help users organize tasks efficiently. Users can create, edit, and delete tasks, set deadlines, and prioritize their workload. The app features a clean and intuitive UI that allows for easy drag-and-drop functionality, providing a seamless experience for managing daily activities.",
         category: "Web Development",
         image: {
-            src: Image,
+            src: ChoosenPick,
         },
         github: "https://github.com/shawaiz-khan",
         demo: "https://devdigitals.org/",
@@ -57,7 +70,7 @@ const projects: IProjects[] = [
         description: "A dashboard for tracking social media analytics, developed with React and Chart.js. This dashboard provides a comprehensive view of user engagement across multiple platforms, including follower growth, post performance, and audience demographics. It is designed to help businesses and individuals analyze their social media strategies effectively.",
         category: "Web Development",
         image: {
-            src: Image,
+            src: QuranTune,
         },
         github: "https://github.com/shawaiz-khan",
         demo: "https://devdigitals.org/",
@@ -67,7 +80,7 @@ const projects: IProjects[] = [
         description: "A showcase of graphic design work, including logos, branding projects, and marketing materials. This portfolio highlights my skills in Adobe Creative Suite and demonstrates my ability to create visually appealing designs that effectively communicate brand messages. It includes case studies that outline the design process and the impact of each project.",
         category: "Graphic Design",
         image: {
-            src: Image,
+            src: HomeInternetPlains,
         },
         github: "https://github.com/shawaiz-khan",
         demo: "https://devdigitals.org/",
@@ -77,7 +90,7 @@ const projects: IProjects[] = [
         description: "A case study of a UI/UX design project, showcasing wireframes, user flows, and prototypes. This project illustrates my design thinking process, from initial research and user interviews to final design and usability testing. It emphasizes the importance of user-centered design and how I implemented feedback to create a polished product.",
         category: "Graphic Design",
         image: {
-            src: Image,
+            src: HighAccessRentals,
         },
         github: "https://github.com/shawaiz-khan",
         demo: "https://devdigitals.org/",
@@ -87,7 +100,7 @@ const projects: IProjects[] = [
         description: "A fully responsive website for a restaurant, featuring a menu, reservation system, and contact information. The site is designed to attract customers with mouth-watering visuals and an easy-to-navigate layout. It includes a blog section for sharing recipes and news, enhancing customer engagement and online presence.",
         category: "Web Development",
         image: {
-            src: Image,
+            src: SultanOfMartialArt,
         },
         github: "https://github.com/shawaiz-khan",
         demo: "https://devdigitals.org/",
@@ -97,7 +110,7 @@ const projects: IProjects[] = [
         description: "A portfolio website for a photographer, showcasing galleries, contact forms, and booking options. The design focuses on high-quality images and an intuitive layout, allowing potential clients to view the photographer's work easily. It includes sections for testimonials and services offered, promoting user interaction and inquiries.",
         category: "Web Development",
         image: {
-            src: Image,
+            src: DigitalWatch,
         },
         github: "https://github.com/shawaiz-khan",
         demo: "https://devdigitals.org/",
@@ -107,7 +120,7 @@ const projects: IProjects[] = [
         description: "A real-time chat application built with Node.js and Socket.io. This application allows users to communicate instantly through text messages, share images, and create chat rooms. It features user authentication and the ability to create private chats, making it a versatile tool for personal or professional use.",
         category: "Web Development",
         image: {
-            src: Image,
+            src: ReactCalculator,
         },
         github: "https://github.com/shawaiz-khan",
         demo: "https://devdigitals.org/",
@@ -117,7 +130,7 @@ const projects: IProjects[] = [
         description: "A responsive portfolio website showcasing my projects and skills. This site is designed to provide visitors with a comprehensive view of my work, including detailed case studies, technology stacks used, and the design process behind each project. It features smooth navigation, an engaging layout, and a clean aesthetic to highlight my creativity and attention to detail.",
         category: "Web Development",
         image: {
-            src: Image,
+            src: PharmacyManagementSystem,
         },
         github: "https://github.com/shawaiz-khan",
         demo: "https://devdigitals.org/",
@@ -127,7 +140,7 @@ const projects: IProjects[] = [
         description: "An online store built with React and Node.js, featuring a robust shopping cart and payment integration. Users can browse products, add them to their cart, and complete their purchases with ease. The store is optimized for mobile devices and includes user authentication, allowing customers to create accounts, track orders, and manage their profiles seamlessly.",
         category: "Web Development",
         image: {
-            src: Image,
+            src: WeatherApp,
         },
         github: "https://github.com/shawaiz-khan",
         demo: "https://devdigitals.org/",
@@ -137,7 +150,7 @@ const projects: IProjects[] = [
         description: "A blogging platform developed using Next.js that allows users to create, edit, and manage their posts. The platform features a rich text editor, user authentication, and a comment section for reader engagement. Users can categorize their blogs, add tags, and enhance their content with images and videos, providing a user-friendly interface for both writers and readers.",
         category: "Web Development",
         image: {
-            src: Image,
+            src: TicTacToe,
         },
         github: "https://github.com/shawaiz-khan",
         demo: "https://devdigitals.org/",
@@ -147,7 +160,7 @@ const projects: IProjects[] = [
         description: "A task management application built with React and Tailwind CSS to help users organize tasks efficiently. Users can create, edit, and delete tasks, set deadlines, and prioritize their workload. The app features a clean and intuitive UI that allows for easy drag-and-drop functionality, providing a seamless experience for managing daily activities.",
         category: "Web Development",
         image: {
-            src: Image,
+            src: CounterApp,
         },
         github: "https://github.com/shawaiz-khan",
         demo: "https://devdigitals.org/",
@@ -157,7 +170,7 @@ const projects: IProjects[] = [
         description: "A dashboard for tracking social media analytics, developed with React and Chart.js. This dashboard provides a comprehensive view of user engagement across multiple platforms, including follower growth, post performance, and audience demographics. It is designed to help businesses and individuals analyze their social media strategies effectively.",
         category: "Web Development",
         image: {
-            src: Image,
+            src: ZenithIDE,
         },
         github: "https://github.com/shawaiz-khan",
         demo: "https://devdigitals.org/",
